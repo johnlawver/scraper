716301e0c6bd8fd626dfb95f2ca366a47fd6275a
johnlawver <jlawver@iherb.net>
2019-09-16 16:25:57 -0700

    Error handling and array functions

--- request.js
+++ request.js
@@ -1,54 +1,56 @@
 const fs = require("fs");
 
+//parse json file
 let request = JSON.parse(fs.readFileSync("response.json"));
+
+//create an emty object to hold new hire info
 let newHire = new Object();
 
+//Create a function that takes firtname last name and employee type to generate email and staging ou
 let createEmail = newHire => {
   if (newHire.teamMemberType === "Core") {
     domain = "@iherb.com";
+    ////////////////////////////////////////confirm OU before using//////////////////
+    newHire.ou = 'new Hire Pending 2FA'
   } else {
     domain = "@external.iherb.com";
+    newHire.ou = 'external users/2fa not enforced (external)'
   }
 
-  return `${newHire.firstName}.${newHire.lastName}${domain}`.toLocaleLowerCase();
+  newHire.email = `${newHire.firstName}.${newHire.lastName}${domain}`.toLocaleLowerCase();
 };
-//let hardwareArray = request.fields.customfield_10133;
 
-newHire.firstName = request.fields.customfield_10124;
-newHire.lastName = request.fields.customfield_10125;
-newHire.startDate = request.fields.customfield_10129;
-newHire.title = request.fields.customfield_10127;
-newHire.manager = request.fields.customfield_10128;
-newHire.personToMirror = request.fields.customfield_10132;
-newHire.vpn = request.fields.customfield_10136.value;
-newHire.emailAccess = request.fields.customfield_10141.value;
-newHire.cubicle = request.fields.customfield_10140;
-newHire.labelsArray = request.fields.labels;
-newHire.location = request.fields.customfield_10104.value;
-newHire.department = request.fields.customfield_10109.value;
-newHire.teamMemberType = request.fields.customfield_10111.value;
-//POC Needs a foreach loop to get all hardware when more than one is selected
-newHire.hardwareArray = request.fields.customfield_10133[0].value;
-newHire.classification = request.fields.customfield_10131.value;
-newHire.email = createEmail(newHire);
+//extract value fields from software and hardware arrays
+let extractValues = (arrayItem) => {
+  //ensure item is not null
+  if (arrayItem) {
+    let valueArray = []
+    arrayItem.forEach(element => {
+      valueArray.push(element.value)
+    });
+    return valueArray;
+  }
+}
+
+//Define object fields with basic logic to remove null fields 
+newHire.firstName =  request.fields.customfield_10124 ?request.fields.customfield_10124 : 'NOT DEFINED'
+newHire.lastName = request.fields.customfield_10125? request.fields.customfield_10125 : 'NOT DEFINED'
+newHire.startDate = request.fields.customfield_10129? request.fields.customfield_10129: 'NOT DEFINED'
+newHire.title = request.fields.customfield_10127? request.fields.customfield_10127: 'NOT DEFINED'
+newHire.manager = request.fields.customfield_10128? request.fields.customfield_10128:'NOT DEFINED'
+newHire.personToMirror = request.fields.customfield_10132? request.fields.customfield_10132:'None'
+newHire.vpn = request.fields.customfield_10136? request.fields.customfield_10136.value:'No'
+newHire.emailAccess = request.fields.customfield_10141.value? request.fields.customfield_10141.value:'No'
+newHire.cubicle = request.fields.customfield_10140? request.fields.customfield_10140:'NOT DEFINED'
+newHire.labelsArray = request.fields.labels? request.fields.labels:'NOT DEFINED'
+newHire.location = request.fields.customfield_10104.value? request.fields.customfield_10104.value: 'NOT DEFINED'
+newHire.department = request.fields.customfield_10109.value? request.fields.customfield_10109.value:'NOT DEFINED'
+newHire.teamMemberType = request.fields.customfield_10111? request.fields.customfield_10111.value : 'Core';
+newHire.classification = request.fields.customfield_10131? request.fields.customfield_10131.value : 'NOT DEFINED'
+newHire.hardwareArray = extractValues(request.fields.customfield_10133)
+newHire.software = extractValues(request.fields.customfield_10135)
+createEmail(newHire);
 
 let getEmployeeObject = () => newHire;
 
 module.exports = getEmployeeObject;
-
-//console.log(newHire);
-//console.log(newHire);
-// console.log(labelsArray);
-// console.log(location);
-// console.log(department);
-// console.log(teamMemberType);
-// console.log(firstName);
-// console.log(lastName);
-// console.log(title);
-// console.log(manager);
-console.log(newHire.hardwareArray);
-// console.log(cubicle);
-// console.log(classification);
-// console.log(`vpn: ${vpn}`);
-// console.log(`email: ${emailAccess}`);
-// console.log(startDate);
