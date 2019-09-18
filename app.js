e27b059b850a1ea938750966e411b52f80b19fd9
johnlawver <jlawver@iherb.net>
2019-09-18 15:52:10 -0700

    updated format for shanghai and update bulkcsv output

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const emp = getEmployeeObject()
 //console.log(emp)
 const csvLine = `${emp.name}, ${emp.email}, ${password}, ${emp.adName}, ${password}` + "\r\n";
 const bulkCSV =
-  `NEW, ${emp.email}, , ${emp.firstName}, ${emp.lastName}, ${password}, , YES, , SHOW` +
+  `NEW, ${emp.email}, , ${emp.firstName}, ${emp.lastName}, ${password}, , YES, , SHOW, , , , ,${emp.ou}, , , , ${emp.title}, ${emp.department}, ${emp.manager}` +
   "\r\n";
 
   fs.appendFile("./output/tempPwLog.csv", `${csvLine}`, function(err) {
--- request.js
+++ request.js
@@ -14,12 +14,24 @@ let createEmail = newHire => {
   } else {
     domain = '@external.iherb.com';
     newHire.ou = 'external users/2fa not enforced (external)';
+  } if (newHire.location == 'Shanghai Puxi'){
+    newHire.adName = `${newHire.preferredName}.${newHire.lastName.split(' ').join('')}`.toLocaleLowerCase();
+    newHire.email = `${newHire.adName}${domain}`;
+  } else {
+    newHire.adName = `${newHire.firstName.split(' ').join('')}.${newHire.lastName.split(' ').join('')}`.toLocaleLowerCase();
+    newHire.email = `${newHire.adName}${domain}`;
   }
-  newHire.adName = `${newHire.firstName
-    .split(' ')
-    .join('')}.${newHire.lastName.split(' ').join('')}`.toLocaleLowerCase();
-  newHire.email = `${newHire.adName}${domain}`;
 };
+
+let cleanPreferredName = (prefName) => {
+  let preferredArray = prefName.split(' ')
+  preferredArray.forEach((e,i) =>{
+    if (e.toLocaleLowerCase() == newHire.firstName.toLocaleLowerCase() || e.toLocaleLowerCase() == newHire.lastName.toLocaleLowerCase()) {
+      preferredArray.splice(i,1)
+    }
+  })
+  return preferredArray.join('')
+}
 
 let createName = newHire => {
   let name = '';
@@ -29,6 +41,8 @@ let createName = newHire => {
       newHire.preferredName.toLocaleLowerCase()
   ) {
     name = `${newHire.firstName} ${newHire.lastName}`;
+  } else if (newHire.location == 'Shanghai Puxi') {
+    name = `${newHire.firstName} (${newHire.preferredName}) ${newHire.lastName}`;
   } else {
     name = `${newHire.preferredName} (${newHire.firstName}) ${newHire.lastName}`;
   }
@@ -50,55 +64,26 @@ let extractValues = arrayItem => {
 };
 
 //Define object fields with basic logic to remove null fields
-newHire.firstName = request.fields.customfield_10124
-  ? request.fields.customfield_10124
-  : 'NOT DEFINED';
-newHire.lastName = request.fields.customfield_10125
-  ? request.fields.customfield_10125
-  : 'NOT DEFINED';
-newHire.preferredName = request.fields.customfield_10126
-  ? request.fields.customfield_10126
-  : 'None';
-newHire.name = createName(newHire);
-newHire.startDate = request.fields.customfield_10129
-  ? request.fields.customfield_10129
-  : 'NOT DEFINED';
-newHire.title = request.fields.customfield_10127
-  ? request.fields.customfield_10127
-  : 'NOT DEFINED';
-newHire.manager = request.fields.customfield_10128
-  ? request.fields.customfield_10128
-  : 'NOT DEFINED';
-newHire.personToMirror = request.fields.customfield_10132
-  ? request.fields.customfield_10132
-  : 'None';
-newHire.vpn = request.fields.customfield_10136
-  ? request.fields.customfield_10136.value
-  : 'No';
-newHire.emailAccess = request.fields.customfield_10141.value
-  ? request.fields.customfield_10141.value
-  : 'No';
-newHire.cubicle = request.fields.customfield_10140
-  ? request.fields.customfield_10140
-  : 'NOT DEFINED';
-newHire.labelsArray = request.fields.labels
-  ? request.fields.labels
-  : 'NOT DEFINED';
-newHire.location = request.fields.customfield_10104.value
-  ? request.fields.customfield_10104.value
-  : 'NOT DEFINED';
-newHire.department = request.fields.customfield_10109.value
-  ? request.fields.customfield_10109.value
-  : 'NOT DEFINED';
-newHire.teamMemberType = request.fields.customfield_10111
-  ? request.fields.customfield_10111.value
-  : 'Core';
-newHire.classification = request.fields.customfield_10131
-  ? request.fields.customfield_10131.value
-  : 'NOT DEFINED';
+newHire.firstName = request.fields.customfield_10124 ? request.fields.customfield_10124 : 'NOT DEFINED';
+newHire.lastName = request.fields.customfield_10125 ? request.fields.customfield_10125 : 'NOT DEFINED';
+newHire.location = request.fields.customfield_10104.value? request.fields.customfield_10104.value : '';
+newHire.startDate = request.fields.customfield_10129 ? request.fields.customfield_10129 : 'NOT DEFINED';
+newHire.title = request.fields.customfield_10127 ? request.fields.customfield_10127 : '';
+newHire.manager = request.fields.customfield_10128 ? request.fields.customfield_10128 : 'NOT DEFINED';
+newHire.personToMirror = request.fields.customfield_10132 ? request.fields.customfield_10132 : 'None';
+newHire.vpn = request.fields.customfield_10136 ? request.fields.customfield_10136.value : 'No';
+newHire.emailAccess = request.fields.customfield_10141.value ? request.fields.customfield_10141.value : 'No';
+newHire.cubicle = request.fields.customfield_10140 ? request.fields.customfield_10140 : 'NOT DEFINED';
+newHire.labelsArray = request.fields.labels ? request.fields.labels : 'NOT DEFINED';
+newHire.department = request.fields.customfield_10109.value ? request.fields.customfield_10109.value : '';
+newHire.teamMemberType = request.fields.customfield_10111 ? request.fields.customfield_10111.value : 'Core';
+newHire.classification = request.fields.customfield_10131 ? request.fields.customfield_10131.value : 'NOT DEFINED';
 newHire.hardwareArray = extractValues(request.fields.customfield_10133);
 newHire.software = extractValues(request.fields.customfield_10135);
+newHire.preferredName = request.fields.customfield_10126 ? cleanPreferredName(request.fields.customfield_10126) : 'None';
+newHire.name = createName(newHire);
 createEmail(newHire);
+
 
 let getEmployeeObject = () => newHire;
 
