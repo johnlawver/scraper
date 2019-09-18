729a4b212372ca7ea69a7f08e0ae729b781c29a7
johnlawver <jlawver@iherb.net>
2019-09-17 17:00:25 -0700

    Add ou and name logic

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-var fs = require("fs");
-var generatePassword = require("password-generator");
-const args = process.argv.slice(2);
-const firstName = args[0];
-const lastName = args[1];
-const name = `${firstName} ${lastName}`;
+const fs = require("fs");
+const generatePassword = require("password-generator");
+const getEmployeeObject = require("./request");
 const password = generatePassword(9, false);
-const ad = `${firstName}.${lastName}`;
-const email = `${ad}@external.iherb.com`.toLocaleLowerCase();
 
-const csvLine = `${name}, ${email}, ${password}` + "\r\n";
+const emp = getEmployeeObject()
+
+//console.log(emp)
+const csvLine = `${emp.name}, ${emp.email}, ${password}, ${emp.adName}, ${password}` + "\r\n";
 const bulkCSV =
-  `NEW, ${email}, , ${firstName}, ${lastName}, ${password}, , YES, , SHOW` +
+  `NEW, ${emp.email}, , ${emp.firstName}, ${emp.lastName}, ${password}, , YES, , SHOW` +
   "\r\n";
 
-fs.appendFile("log.csv", `${csvLine}`, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`${firstName} Added`);
-  }
-});
-
-fs.appendFile("bulk.csv", `${bulkCSV}`, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    // done
-  }
-});
-
-// console.log('FIRST NAME')
-// console.log(firstName)
-// console.log('')
-// console.log('LAST NAME')
-// console.log(lastName)
-// console.log('')
-// console.log('EMAIL')
-// console.log(email);
-// console.log('')
-// console.log('PASSWORD')
-// console.log(password);
+  fs.appendFile("./output/tempPwLog.csv", `${csvLine}`, function(err) {
+    if (err) {
+      console.log(err);
+    } 
+  });
+  
+  fs.appendFile("./output/bulkUpload.csv", `${bulkCSV}`, function(err) {
+    if (err) {
+      console.log(err);
+    } 
+  });
--- request.js
+++ request.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+
 //parse json file
 let request = JSON.parse(fs.readFileSync("response.json"));
 
@@ -10,15 +11,24 @@ let newHire = new Object();
 let createEmail = newHire => {
   if (newHire.teamMemberType === "Core") {
     domain = "@iherb.com";
-    ////////////////////////////////////////confirm OU before using//////////////////
-    newHire.ou = 'new Hire Pending 2FA'
+    newHire.ou = '2fa not enforced/new users pending 2fa activation'
   } else {
     domain = "@external.iherb.com";
     newHire.ou = 'external users/2fa not enforced (external)'
   }
-
-  newHire.email = `${newHire.firstName}.${newHire.lastName}${domain}`.toLocaleLowerCase();
+  newHire.adName = `${newHire.firstName}.${newHire.lastName}`.toLocaleLowerCase();
+  newHire.email = `${newHire.adName}${domain}`
 };
+
+let createName = newHire => {
+  let name =''
+  if (newHire.preferredName == "None" || newHire.firstName.toLocaleLowerCase() == newHire.preferredName.toLocaleLowerCase()) {
+    name = `${newHire.firstName} ${newHire.lastName}`
+  } else {
+    name = `${newHire.preferredName} (${newHire.firstName}) ${newHire.lastName}`
+  }
+  return name
+}
 
 //extract value fields from software and hardware arrays
 let extractValues = (arrayItem) => {
@@ -35,6 +45,8 @@ let extractValues = (arrayItem) => {
 //Define object fields with basic logic to remove null fields 
 newHire.firstName =  request.fields.customfield_10124 ?request.fields.customfield_10124 : 'NOT DEFINED'
 newHire.lastName = request.fields.customfield_10125? request.fields.customfield_10125 : 'NOT DEFINED'
+newHire.preferredName = request.fields.customfield_10126? request.fields.customfield_10126 : 'None'
+newHire.name = createName(newHire);
 newHire.startDate = request.fields.customfield_10129? request.fields.customfield_10129: 'NOT DEFINED'
 newHire.title = request.fields.customfield_10127? request.fields.customfield_10127: 'NOT DEFINED'
 newHire.manager = request.fields.customfield_10128? request.fields.customfield_10128:'NOT DEFINED'
